8dcc983d6f8c7510d5eddf7ddbbb2f997f5c10c2
zhou.shidong <zhou.shidong@immomo.com>
2018-05-05 10:17:10 +0800

    Question 6(surface/space point interpolating)

---
+++ course_algrithm/spip.hpp
@@ -0,0 +1,14 @@
+//
+//  spip.hpp
+//  course_algrithm
+//
+//  Created by Raii on 05/05/2018.
+//  Copyright © 2018 Raii. All rights reserved.
+//
+
+#ifndef spip_hpp
+#define spip_hpp
+
+#include <stdio.h>
+
+#endif /* spip_hpp */
